f8648b76ecaf315509e3d0d88cf8b456f3328505
James Hibbard <james.hibbard@sitepoint.com>
2015-11-04 15:27:41 +0100

    Formatting

--- es5/app.js
+++ es5/app.js
@@ -1,11 +1,11 @@
 var UserComponent = ng.Component({
-        selector: 'user-name'
-    })
-    .View({
-        template: '<h1>My name is {{ name }}</h1>'
-    })
-    .Class({
-        constructor: function(){
-            this.name="Tom Greco";
-        }
-    });
+  selector: 'user-name'
+})
+.View({
+  template: '<h1>My name is {{ name }}</h1>'
+})
+.Class({
+  constructor: function(){
+    this.name="Tom Greco";
+  }
+});
